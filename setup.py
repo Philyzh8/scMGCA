fdfc9e380351621aa8ed87ee56ee9683e5229c2a
Zhuohan Yu <65069252+Philyzh8@users.noreply.github.com>
2022-09-27 23:00:14 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,14 +11,16 @@ from setuptools import setup
 
 setup(
     name='scMGCA',
-    version='1.0.0',
+    version='1.0.7',
     description='Topological Identification and Interpretation for High-throughput Single-cell Gene Regulation Elucidation across Multiple Platforms using scMGCA',
     author='Zhuohan Yu',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author_email="zhuohan20@mails.jlu.edu.cn",
     packages=['scMGCA'],
     url='https://github.com/Philyzh8/scMGCA',
-    license='Apache License 2.0',
-    classifiers=['License :: OSI Approved :: Apache Software License',
+    license='MIT',
+    classifiers=['Operating System :: OS Independent',
                 'Topic :: Scientific/Engineering :: Artificial Intelligence',
-                 'Programming Language :: Python :: 3.5'],
+                 'Programming Language :: Python :: 3.6'],
 )
